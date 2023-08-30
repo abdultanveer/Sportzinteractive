386ea4c74dc5648e3dc3c0e6e7422dfc48944a05
Abdul <abdul.tanveer@gmail.com>
2023-08-30 15:09:14 +0530

    diceroll - 1 & 2

--- app/src/main/java/com/example/sportzinteractive/MainActivity.kt
+++ app/src/main/java/com/example/sportzinteractive/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.sportzinteractive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.sportzinteractive.model.Dice
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //taking handle of the button
+        val rollButton: Button = findViewById(R.id.btnRoll)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.tvFirst)
+        resultTextView.text = diceRoll.toString()
     }
 
     fun clickHandler(view: View) {
---
+++ app/src/main/java/com/example/sportzinteractive/Student.java
@@ -0,0 +1,16 @@
+package com.example.sportzinteractive;
+
+public class Student {
+    String name;
+    String address;
+    int phno;
+
+
+    public static void main(String[] args) {
+        Student abdul; //declaration
+        abdul = new Student(); //instantiation
+        abdul.name = "ansari"; //initialization
+
+    }
+}
+

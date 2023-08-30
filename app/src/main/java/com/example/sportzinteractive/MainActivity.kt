e660315a322bb18e86f756173833d5ab9d8d59a2
Abdul <abdul.tanveer@gmail.com>
2023-08-30 15:46:49 +0530

    diceroll - 3 & 4

--- app/src/main/java/com/example/sportzinteractive/MainActivity.kt
+++ app/src/main/java/com/example/sportzinteractive/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.sportzinteractive.model.Dice
@@ -25,6 +26,19 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+
         val resultTextView: TextView = findViewById(R.id.tvFirst)
         resultTextView.text = diceRoll.toString()
     }

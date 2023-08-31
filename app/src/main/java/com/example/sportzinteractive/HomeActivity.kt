9271b7f4626f2addd753bb32676db761b97b4b15
Abdul <abdul.tanveer@gmail.com>
2023-08-31 11:47:49 +0530

    activities, passing data b/w activities - implicit intents

--- app/src/main/java/com/example/sportzinteractive/HomeActivity.kt
+++ app/src/main/java/com/example/sportzinteractive/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sportzinteractive
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.view.View
 import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
@@ -19,5 +22,25 @@ class HomeActivity : AppCompatActivity() {
         var mainIntent = Intent(this,MainActivity::class.java)
         mainIntent.putExtra("skey","rolling dice")
         startActivity(mainIntent)
+    }
+
+    fun launchDialer(view: View) {
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:1234567"))
+        startActivity(dIntent)
+    }
+
+    fun setAlarm(view: View) {
+        createAlarm("time for sports",11,48)
+    }
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+       // if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+       // }
     }
 }

823ad504f844383cc620741ced3df1a844213b78
Abdul <abdul.tanveer@gmail.com>
2023-09-01 17:26:05 +0530

    roomdb part-4 textview observing live data

--- app/src/main/java/com/example/sportzinteractive/RoomActivity.kt
+++ app/src/main/java/com/example/sportzinteractive/RoomActivity.kt
@@ -3,18 +3,24 @@ package com.example.sportzinteractive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.viewModels
 import com.example.sportzinteractive.database.Word
 
 
 class RoomActivity : AppCompatActivity() {
-
+lateinit var observerTextView: TextView
     private val wordViewModel: WordViewModel by viewModels {
         WordViewModelFactory((application as WordsApplication).repository)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
+        observerTextView = findViewById(R.id.tvObserver)
+
+        wordViewModel.allWords.observe(this){words ->
+            observerTextView.setText(words.get(words.size-1).word)
+        }
     }
 
     fun putData(view: View) {

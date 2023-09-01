6f7ee6fe393d732c3bca79603902df161623f387
Abdul <abdul.tanveer@gmail.com>
2023-09-01 13:12:24 +0530

    connecting internet - part1 dependencies

---
+++ app/src/main/java/com/example/sportzinteractive/NetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sportzinteractive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_net)
+    }
+}
